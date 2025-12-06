37aac500e45a5db3565499017644a7adfa079251
oppo77 <131964159+oppo77@users.noreply.github.com>
2025-12-06 22:46:46 +0800

    Update armv8.mk

--- xiguapi/custom/target/linux/rockchip/image/armv8.mk
+++ xiguapi/custom/target/linux/rockchip/image/armv8.mk
@@ -193,18 +193,19 @@ define Device/radxa_rock-3b
 endef
 TARGET_DEVICES += radxa_rock-3b
 
+
 define Device/nlnet_xiguapi-v3
   DEVICE_VENDOR := NLNET
   DEVICE_MODEL := Xiguapi V3
   SOC := rk3568
-  DEVICE_DTS_DIR := ../dts/rk3568
-  DEVICE_DTS := rk3568-xiguapi-v3
+  DEVICE_DTS := rockchip/rk3568-xiguapi-v3
   UBOOT_DEVICE_NAME := rk3568-xiguapi-v3
   KERNEL_LOADADDR := 0x04000000
   BOOT_SCRIPT := rockchip
   DEVICE_PACKAGES := kmod-r8169
 endef
 TARGET_DEVICES += nlnet_xiguapi-v3
+
 
 define Device/radxa_rock-3c
   DEVICE_VENDOR := Radxa
